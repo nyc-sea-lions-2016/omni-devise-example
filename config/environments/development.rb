61efb8b1b3cc2e5d14c65d627029f79f776ebb26
Steven Cassidy <stevecass@devbootcamp.com>
2016-04-19 09:38:25 -0400

    Configure ActionMailert to user smtp:localhost:1025

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,9 +14,16 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
 
-  # Print deprecation notices to the Rails logger.
+  # SMTP settings for mailcatcher
+  config.action_mailer.smtp_settings = {
+   :address              => "localhost",
+   :port                 => 1025
+  }
+    # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
   # Raise an error on page load if there are pending migrations.
@@ -35,7 +42,6 @@ Rails.application.configure do
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
